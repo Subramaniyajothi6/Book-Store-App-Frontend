75bc9371dbbdef3dc3ac34dea2207ad134b12bd1
Subramaniya Jothi <subramaniyajothi2002@gmail.com>
2025-05-01 11:41:56 +0530

    Fixed dependency conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import {  AuthProvider } from "./context/AuthContext"
-import "./app.css"
+import "./App.css"
 
 function App() {
 
