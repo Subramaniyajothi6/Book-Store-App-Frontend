0e8eae134d3bcc74b214864307bf53d980eee317
Subramaniya Jothi <subramaniyajothi2002@gmail.com>
2025-05-02 00:50:38 +0530

    Updated base URL

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return import.meta.env.VITE_BASE_URL || "localhost:5000"; 
+    return import.meta.env.VITE_BASE_URL || "http://localhost:5000"; 
 }
 
 export default getBaseUrl;
