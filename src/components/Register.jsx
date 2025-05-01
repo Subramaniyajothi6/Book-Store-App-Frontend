8d16018e6c33f7efa71d47c2f4c53256927a7f68
Subramaniya Jothi <subramaniyajothi2002@gmail.com>
2025-05-01 11:34:40 +0530

    Fixed dependency conflicts

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -96,69 +96,6 @@ const Register = () => {
 </div>
 </div>
 
-// {/* <div className="relative h-screen w-full flex items-center justify-center">
-//       {/* Canvas Background */}
-//       <div className="absolute top-0 left-0 w-full h-full -z-10">
-//         <Velustro />
-//       </div>
-
-//       {/* Login Box */}
-//       <div className="relative z-20 w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 py-6 bg-white/80">
-//         <h2 className="text-2xl font-semibold mb-4">Please Register</h2>
-
-//         <form onSubmit={handleSubmit(onSubmit)}>
-//           <div className="mb-4">
-//             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-//               Email
-//             </label>
-//             <input {...register("email", { required: true })}
-//               type="email"
-//               name="email"
-//               id="email"
-//               placeholder="Enter your email"
-//               className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-//             />
-//           </div>
-
-//           <div className="mb-4">
-//             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-//               Password
-//             </label>
-//             <input {...register("password", { required: true })}
-//               type="password"
-//               name="password"
-//               id="password"
-//               placeholder="Enter your Password"
-//               className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-//             />
-//           </div>
-
-//           {message && <p className="text-red-500 text-xs italic mb-3">{message}</p>}
-
-//           <div>
-//             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none">
-//               Sign Up
-//             </button>
-//           </div>
-//         </form>
-
-//         <p className="text-sm mt-4">
-//           Already have an account? <Link to="/login" className="text-blue-500 hover:text-blue-700">Login</Link>
-//         </p>
-
-//         <div className="mt-5">
-//           <button onClick={handleGoogleSignIn}
-//             className="w-full flex justify-center items-center gap-3 bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none">
-//             <FaGoogle />
-//             Sign Up with Google
-//           </button>
-//         </div>
-
-//         <p className="text-center text-sm mt-4 text-gray-500">©2025 Book Store. All rights reserved.</p>
-//       </div>
-//     </div> */}
-
-
   )
 }
 
