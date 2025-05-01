f0bd3f6840ba4ddf00c871fb3dc2ed3f36dde8d9
Subramaniya Jothi <subramaniyajothi2002@gmail.com>
2025-05-02 00:23:20 +0530

    Updated base URL

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return import.meta.env.VITE_BASE_URL || "http://localhost:5000"; // Fallback to localhost if not defined
+    return import.meta.env.VITE_BASE_URL || "https://book-store-app-backend-livid.vercel.app"; 
 }
 
 export default getBaseUrl;
